e0e7dfbd8c93c1a9520b728906bbff7abf92af2c
Dectecter <87181456+Dectecter@users.noreply.github.com>
2024-05-05 21:36:16 +0300

    USB 1 restart ckick file

---
+++ Restart-device USB 1.ps1
@@ -0,0 +1,11 @@
+﻿pnputil /enum-devices /connected /class USB | Sort-Object -Descending | Select-Object -Last 30 | Select-String -Pattern "USB" | Select-String -Pattern "ИД экземпляра" >> D:\Tdead.txt
+(Get-Content -Path D:\Tdead.txt) |
+    ForEach-Object {$_ -Replace 'ИД экземпляра:                ', ''} |
+        Set-Content -Path D:\Tdead.txt
+Get-Content -Path D:\Tdead.txt
+foreach ($line in Get-Content D:\Tdead.txt) {
+    if ($line -match $regex) {
+        pnputil /restart-device "$line"
+    }
+}
+Start-Sleep -Seconds 5
